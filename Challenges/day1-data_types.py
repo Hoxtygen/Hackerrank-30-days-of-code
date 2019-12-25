b528beef953d6418761951fdab5e4d1eff4c915c
Wasiu Idowu <hoxtygen@live.com>
2019-12-25 16:13:09 +0100

    Add day1 challenge

---
+++ Challenges/day1-data_types.py
@@ -0,0 +1,18 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+num = None
+float_num = None
+s2 = None
+# Read and save an integer, double, and String to your variables.
+num = int(input())
+float_num = float(input())
+s2 = str(input())
+# Print the sum of both integer variables on a new line.
+print(i + num)
+# Print the sum of the double variables on a new line.
+print(d + float_num)
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+print(s + s2)
